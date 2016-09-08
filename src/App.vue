0cc201f516475c8cae9d8c0165f63f4ebde7f3e2
Max Mathieu <max@voicevibes.co>
2016-09-08 10:32:52 -0400

    Snapshot

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul>
+    <input v-model="newTodo" placeholder="new todo" @keyup.enter="addTodo">
+    <li v-for="(todo, index) in todos">
+      <item :todo="todo"></item>
+      <button @click="removeTodo(index)">X</button>
+    </li>
+  </ul>
+</template>
+
+<style>
+  body, input, button {
+    font-family: Arial;
+    font-size: 30px;
+    margin: 10px;
+  }
+  button {
+    color: #000;
+  }
+</style>
+
+<script>
+  import Item from './Item.vue';
+
+  export default {
+    components: { Item },
+    data () {
+      return {
+        newTodo: '',
+        todos: [
+          { text: 'One item', added: new Date() }
+        ]
+      }
+    },
+    methods: {
+      addTodo: function () {
+        const text = this.newTodo.trim();
+        if (text) {
+          this.todos.push({
+            text: text,
+            added: new Date()
+          });
+          this.newTodo = '';
+        }
+      },
+      removeTodo: function (index) {
+        this.todos.splice(index, 1)
+      }
+    }
+  }
+</script>
---
+++ src/Item.vue
@@ -0,0 +1,30 @@
+<template>
+  <span>
+    <span v-if="editMode"><input v-model="todo.text" @keyup.enter="editMode=false"></span>
+    <span v-else>{{ todo.text }} <button @click="editMode=true">Edit</button></span>
+    <br>
+    <small>{{ added }}</small>
+  </span>
+</template>
+
+<style scoped>
+  button {
+    color: #f00;
+  }
+</style>
+
+<script>
+  export default {
+    props: [ 'todo' ],
+    data () {
+      return {
+        editMode: false
+      }
+    },
+    computed: {
+      added() {
+        return this.todo.added.toLocaleString()
+      }
+    }
+  }
+</script>
